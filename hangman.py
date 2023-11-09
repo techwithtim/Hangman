9b111e9082b92315131ba7543eae05583fbad9e4
Luis SOLANO MURILLO <luis.solanomurillo@soprasteria.com>
2023-11-09 12:00:05 +0100

    modif change color

--- hangman.py
+++ hangman.py
@@ -17,7 +17,7 @@ WHITE = (255,255,255)
 RED = (255,0, 0)
 GREEN = (0,255,0)
 BLUE = (0,0,255)
-LIGHT_BLUE = (102,255,255)
+LIGHT_BLUE = (90,255,255)
 
 btn_font = pygame.font.SysFont("arial", 20)
 guess_font = pygame.font.SysFont("monospace", 24)
